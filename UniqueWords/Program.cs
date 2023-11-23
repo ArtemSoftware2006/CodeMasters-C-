a5905b79c11640ed623ca8e1441c6bbea33506c1
Artem <artem.egorov.art2006@gmail.com>
2023-11-23 12:05:43 +0300

    Добавил README

--- UniqueWords/Program.cs
+++ UniqueWords/Program.cs
@@ -3,14 +3,15 @@ using UniqueWords;
 
 UniqueWordService wordService = new UniqueWordService();
 
-List<string> paths = new()
-{
-    Directory.GetCurrentDirectory() + "\\Data\\Беда_одна_не_ходит.txt",
-    Directory.GetCurrentDirectory() + "\\Data\\Начало.txt",
-    Directory.GetCurrentDirectory() + "\\Data\\Tect.txt",
-    Directory.GetCurrentDirectory() + "\\Data\\Хэппи_Энд.txt"
-};
-
-var Words = await wordService.FinduniqueWordFromFilesAsync(paths);
+var Words = await wordService.FinduniqueWordFromFilesAsync(GetPaths());
 
 Words.ToList().ForEach(x => Console.WriteLine(x));
+
+
+//Возращает все файлы из директории Data
+static List<string> GetPaths() 
+{
+    string basePath = Directory.GetCurrentDirectory() + "\\Data";
+
+    return Directory.GetFiles(basePath).ToList();
+}
